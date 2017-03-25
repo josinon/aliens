fb9aed7eec6b9ec4502aba220a206a7fb9b54cf8
jrn4 <jrn4@cin.ufpe.br>
2017-03-25 09:55:35 -0300

    Adição da espaçonave na tela do jogo

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,11 +1,17 @@
 import sys
 import pygame
+from settings import Settings
+from ship import Ship
 
 def run_game():
 	#inicializa o jogo e cria um objeto para a tela
 	pygame.init()
-	screen = pygame.display.set_mode([1200,650])
+	ai_settings = Settings()
+	screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
 	pygame.display.set_caption("Alien Invasion")
+
+	#Cria a espaçonave
+	ship = Ship(screen)
 
 	#Inicia o laço principal do jogo
 	while True:
@@ -14,7 +20,11 @@ def run_game():
 			if event.type == pygame.QUIT:
 				sys.exit()
 
+		#pinta a tela de cinza
+		screen.fill(ai_settings.bg_color)
+		ship.blitme()
+
 		#Deixa a tela mais recente visível
 		pygame.display.flip()
 
-run_game()
+run_game() 
---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings():
+	''' Uma classe para armazenar todas as configurações da 
+	Invasão alienígena.'''
+
+	def __init__(self):
+		#inicializa as configurações do jogo
+		#Configurações de tela
+		self.screen_width = 1200
+		self.screen_height = 650
+		self.bg_color = (230,230,230)
